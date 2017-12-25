80c632ed4dbf88c26e9e9c22c3bc590ea299a829
YanFeng <yanfeng@yodo1.com>
2017-12-25 18:53:03 +0800

    gen Java & Nodejs;

--- thrift-protocol/serviceIDL.thrift
+++ thrift-protocol/serviceIDL.thrift
@@ -4,9 +4,26 @@
 *
 ***/
 
-include "shard.thrift"
-namespace java Poseidon
+namespace java Poseidon.ServiceDefine
 
-service ServiceCaller extends shard.ShardService {
-    Param INVOKE(1:Params params)
+enum ParamType{
+    INT = 1,
+    FLOAT = 2,
+    BOOL = 3,
+    STRING = 4,
+    LIST = 10,
+    MAP = 11
+}
+struct Param{
+    1:ParamType type,
+    2:string body,
+}
+
+struct Params{
+    1:i32 num = 0,
+    2:map<string,Param> paramsBody,
+}
+
+service ServiceCaller{
+    Params INVOKE(1:Params params)
 }
